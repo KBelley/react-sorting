50ff3aad9af599123f3212b4ff67158007b4bf8e
KBelley <kaleb.belley@gmail.com>
2021-01-31 17:12:32 -0500

    Init
    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+function ShuffleArray(arr){
+  return arr.sort(() => Math.random() - 0.5);
+}
+
+function ShuffleButtonClick(e){
+  console.log('ShuffleButtonClick');
+  console.log(e);
+  values = ShuffleArray(values);  
+  console.log(values);
+}
+
+let values = [1,2,3,4,5,6,7,8,9];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <input type='button' value='Shuffle' /> */}
+        <input type='button' onClick={(event) => ShuffleButtonClick(event)} value='Shuffle' />
+
+        {values.map(value => (
+          <div key={value}><p>{value}</p></div>
+        ))}
       </header>
+{/* 
+      <section className="App-section">
+        {values.map(value => (
+          <div><p>{value}</p></div>
+        ))}
+      </section> */}
     </div>
   );
 }
