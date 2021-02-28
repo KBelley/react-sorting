eb42b26ee2fe4d8f6728af66bff24482d19a59bf
KBelley <kaleb.belley@gmail.com>
2021-02-28 15:57:37 -0500

    Functional Merge Sort
    Added functional merge sort algorithm with colour changing and animation.

--- src/App.js
+++ src/App.js
@@ -1,135 +1,117 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import {getBubbleSortAnimations, getMergeSortAnimations} from './sortingAlgorithms.js'
 
-// const myArray = [
-//   0,1,2,3
-// ];
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 5;
 
-// function shuffle(arra1) {
-//   var ctr = arra1.length,
-//   temp,
-//   index;
-// while (ctr > 0) {
-//   index = Math.floor(Math.random() * ctr);
-//   ctr--;
-//   temp = arra1[ctr];
-//   arra1[ctr] = arra1[index];
-//   arra1[index] = temp;
-// }
-// return arra1;
-// }
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 200;
 
-// function App(props) {
-//   const [list, setList] = useState(myArray);
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
 
-//   function handleShuffle() {
-//     setList(shuffle([...list]));
-//     console.log("Shuffle", myArray);
-//   }
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
 
-//   return (
-//     <div>
-//       {list.map((x, index) => (
-//         <div key={x}>
-//           {x}
-//         </div>
-//       ))}
-//       <button onClick={handleShuffle}>Shuffle</button>
-//     </div>
-//   );
-// }
+// This is the lowest value that should be in the array.
+const MIN_VALUE = 1;
 
-// export default App;
+// This is the highest value that should be in the array. Dictates the height of the different bars.
+const MAX_VALUE = 1000;
 
+let myValues = initArray;
 
-
-
-
-
-
-
-
-function ShuffleArray(arra1){
-  // return arr.sort(() => Math.random() - 0.5);
-  var ctr = arra1.length,
-    temp,
-    index;
-  while (ctr > 0) {
-    index = Math.floor(Math.random() * ctr);
-    ctr--;
-    temp = arra1[ctr];
-    arra1[ctr] = arra1[index];
-    arra1[index] = temp;
-  }
-  return arra1;
+function getRandomInt(min, max) { // Get a random number between specified values
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function initArray(){
+  let arr = [];
 
+  for(let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
+    arr.push(getRandomInt(MIN_VALUE, MAX_VALUE));
+  }
 
-let myValues = [1,2,3,4,5,6,7,8,9];
+  return arr;
+}
 
 function App() {
   let [values, setValues] = useState(myValues);
   // let [values, setValues] = useState([{key:1, value:1},{key:2, value:2},{key:3, value:3},{key:4, value:4},{key:5, value:5},{key:6, value:6},{key:7, value:7},{key:8, value:8},{key:9, value:9}]);
 
-  let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (inputArr[j] > inputArr[j + 1]) {
-                let tmp = inputArr[j];
-                inputArr[j] = inputArr[j + 1];
-                inputArr[j + 1] = tmp;
-  
-                setTimeout(() => {
-                  setValues([...inputArr]);
-                }, j * 1000);
-            }
-        }
+  function handleNewArray() {
+    setValues(initArray);
+  }
+
+  function handleMergeSort(){
+    const animations = getMergeSortAnimations(values);
+
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = (100 / MAX_VALUE) * newHeight + '%';
+        }, i * ANIMATION_SPEED_MS);
+      }
     }
-    return inputArr;
-  };
-
-  function handleShuffle() {
-    setValues(ShuffleArray([...values]));
-
-    // setTimeout(() => {
-    //   setValues(ShuffleArray([...values]));
-    // }, 1000);
-    // console.log("Shuffle", myArray);
   }
 
   function handleBubbleSort(){
-    bubbleSort([...values]);
+    const animations = getBubbleSortAnimations(values);
+
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const [barOneIdx, barTwoIdx] = animations[i];
+      const barOneStyle = arrayBars[barOneIdx].style;
+      const barTwoStyle = arrayBars[barTwoIdx].style;
+      // const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+      const color = 'red';
+
+      setTimeout(() => {
+        console.log(i);
+        barOneStyle.backgroundColor = color;
+        barTwoStyle.backgroundColor = color;
+      }, i * ANIMATION_SPEED_MS);
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <input type='button' value='Shuffle' /> */}
-        {/* <input type='button' onClick={(event) => ShuffleButtonClick(event)} value='Shuffle' /> */}
-        {/* <input type='button' onClick={() => {setValues(ShuffleArray(values));console.log(values);}} value='Shuffle' /> */}
-        <input type='button' onClick={handleShuffle} value='Shuffle' />
+        <input type='button' onClick={handleNewArray} value='New Array' />
+        <input type='button' onClick={handleMergeSort} value='Merge Sort' />
         <input type='button' onClick={handleBubbleSort} value='Bubble Sort' />
 
-        {/* <p>
-          {values}
-        </p> */}
-        {/* {values.map((value, index) => (
-          <div key={value}>{value}</div>
-        ))} */}
-        {values.map((x, index) => (
-        <div key={x}>
-          {x}
+        <div className='bar-wrapper'>
+          {values.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{
+                backgroundColor: PRIMARY_COLOR,
+                height: (100 / MAX_VALUE) * value + '%',
+                width: 100 / values.length + '%'
+              }}></div>
+          ))}
         </div>
-      ))}
       </header>
-{/* 
-      <section className="App-section">
-        {values.map(value => (
-          <div><p>{value}</p></div>
-        ))}
-      </section> */}
     </div>
   );
 }
